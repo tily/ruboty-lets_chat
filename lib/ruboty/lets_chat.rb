dd5667a9fbff638cec4b350d0f400928dab9ecd0
tily <tily05@gmail.com>
2015-03-20 21:15:28 +0900

    bundle gem ruboty-lets_chat

---
+++ lib/ruboty/lets_chat.rb
@@ -0,0 +1,7 @@
+require "ruboty/lets_chat/version"
+
+module Ruboty
+  module LetsChat
+    # Your code goes here...
+  end
+end
---
+++ lib/ruboty/lets_chat/version.rb
@@ -0,0 +1,5 @@
+module Ruboty
+  module LetsChat
+    VERSION = "0.0.1"
+  end
+end
